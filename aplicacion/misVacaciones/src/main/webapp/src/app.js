f16e48f6f860c6964ca625853fbf4aa2c2366a52
dalthviz <d.althviz10@uniandes.edu.co>
2016-02-04 13:32:12 -0500

    cambios para el nombre en la barra

--- aplicacion/misVacaciones/src/main/webapp/src/app.js
+++ aplicacion/misVacaciones/src/main/webapp/src/app.js
@@ -3,9 +3,11 @@
     var mod = ng.module("mainApp", ["ui.router"]);
 
 
-        mod.controller("controlador", function($scope, sharedProperties){
+        mod.controller("controlador", function($scope, $rootScope,sharedProperties){
 
-            $scope.usuario= "Daniel";
+            $rootScope.usuario= "Daniel";
+
+            $scope.contrasena = "jojojojo";
             $scope.correo = "correo@gmail.com";
             $scope.objectValue = sharedProperties.getObject();
             $scope.setString = function(newValue) {
@@ -14,6 +16,47 @@
                };
 
         });
+
+         mod.controller("ctrl", function($scope, sharedProperties){
+        $scope.nueva_ciudad = "";
+        $scope.ciudades = [];
+        $scope.nuevo_evento="";
+        $scope.eventos=[];
+        $scope.stringValue = sharedProperties.getString();
+        $scope.objectValue = sharedProperties.getObject().data;
+
+        $scope.agregar = function(nueva_ciudad){
+          $scope.ciudades.push(nueva_ciudad);
+          $scope.nueva_ciudad = "";
+        };
+        $scope.add= function(nuevo_evento){
+            $scope.eventos.push(nuevo_evento);
+            $scope.nuevo_evento="";
+        };
+
+        $scope.detalles = function(nc){
+          window.alert("se muestran abajo en la parte que esta haciendo nicolas");
+        };
+  });
+
+  mod.service('sharedProperties', function() {
+    var stringValue = 'Itinerario';
+    var objectValue = {
+        data: 'test object value'
+    };
+
+    return {
+        getString: function() {
+            return stringValue;
+        },
+        setString: function(value) {
+            stringValue = value;
+        },
+        getObject: function() {
+            return objectValue;
+        }
+    };
+});
 
     mod.config(['$logProvider', function ($logProvider) {
             $logProvider.debugEnabled(true);
@@ -31,55 +74,5 @@
                         templateUrl: "./modules/home/home.html"
                     });
         }]);
-    mod.controller("ctrl", function($scope, sharedProperties){
-        $scope.nueva_ciudad = "";
-        $scope.ciudades = [];
-        $scope.nuevo_evento="";
-        $scope.eventos=[];
-        $scope.stringValue = sharedProperties.getString();
-        $scope.objectValue = sharedProperties.getObject().data;   
 
-        $scope.agregar = function(nueva_ciudad){
-          $scope.ciudades.push(nueva_ciudad);
-          $scope.nueva_ciudad = "";
-        };
-        $scope.add= function(nuevo_evento){
-            $scope.eventos.push(nuevo_evento);
-            $scope.nuevo_evento="";
-        };
-
-        $scope.detalles = function(nc){
-          window.alert("se muestran abajo en la parte que esta haciendo nicolas");
-        };
-  });
-  
-  mod.service('sharedProperties', function() {
-    var stringValue = 'Itinerario';
-    var objectValue = {
-        data: 'test object value'
-    };
-    
-    return {
-        getString: function() {
-            return stringValue;
-        },
-        setString: function(value) {
-            stringValue = value;
-        },
-        getObject: function() {
-            return objectValue;
-        }
-    };
-});
 })(window.angular);
-
-
-var app= angular.module("myApp",[]);
-
-
-app.controller('MainController', ['$scope', function($scope){
-
-
-}]);
-
-
