7f080be64c6d4c886387f71c54a91e7fd338c302
dalthviz <d.althviz10@uniandes.edu.co>
2016-02-28 01:04:04 -0500

    corrección: cambio ciudad por ciudades

--- aplicacion/misVacaciones/src/main/webapp/src/modules/ciudad/ciudad.mod.js
+++ aplicacion/misVacaciones/src/main/webapp/src/modules/ciudad/ciudad.mod.js
@@ -1,6 +1,6 @@
 (function (ng) {
     var mod = ng.module("moduloCiudad", ["ui.bootstrap"]);
 
-    mod.constant("ciudadContext", "api/ciudad");
+    mod.constant("ciudadContext", "api/ciudades");
 
 })(window.angular);
