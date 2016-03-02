84a134e2fab97808d3f6bd5eeedae021aef5b4ee
Maria Camila Remolina Gutierrez <mc.remolina197@uniandes.edu.co>
2016-03-01 21:55:19 -0500

    ctrl de ciudad listo con poca documentación y pequeño arreglo de svc

--- aplicacion/misVacaciones/src/main/webapp/src/modules/ciudad/ciudad.ctrl.js
+++ aplicacion/misVacaciones/src/main/webapp/src/modules/ciudad/ciudad.ctrl.js
@@ -1,135 +1,184 @@
-    (function (ng) {
+(function (ng) {
+
     var mod = ng.module("moduloCiudad");
 
     mod.controller("ciudadCtrl", ["$scope", "ciudadService", function ($scope, svc) {
-            $scope.currentRecord = {};
-            $scope.records = [];
-            $scope.eventos = [];
-            $scope.alerts = [];
 
-            $scope.currentSitio={}; //sitio seleccionado para agregar
-            $scope.fechaSitio= new Date(); //fecha visita del sitio
-            $scope.sitioElegido=""; //sitio elegido por el usurario
-            $scope.sitios=[]; //sitios a mostrar según la ciudad elegida
+        //Ciudad actual
+        $scope.currentCiudad = {};
+        //Ciudades
+        $scope.records = [];
 
+        //Evento actual
+        $scope.currentEvento = {};
+        //Eventos
+        $scope.eventos = [];
 
+        //Sitio actual
+        $scope.currentSitio = {};
+        //Sitios
+        $scope.sitios = [];
 
-            $scope.grabar = function () {
-                svc.agregarEvento();
-                svc.refrescar();
-            };
+        //Hotel actual
+        $scope.currentHotel = {};
+        //Hoteles
+        $scope.hoteles = [];
 
-            $scope.clear = function () {
-                $scope.value = null;
-            };
+        //Alertas
+        $scope.alerts = [];
 
-            $scope.open = function ($event) {
-                $event.preventDefault();
-                $event.stopPropagation();
+        $scope.clear = function () {
+            $scope.value = null;
+        };
 
-                $scope.opened = true;
-            };
+        $scope.open = function ($event) {
+            $event.preventDefault();
+            $event.stopPropagation();
+            $scope.opened = true;
+        };
 
-            //Alertas
-            this.closeAlert = function (index) {
-                $scope.alerts.splice(index, 1);
-            };
+        //Variables para el controlador
+        this.readOnly = false;
+        this.editMode = false;
 
-            function showMessage(msg, type) {
-                var types = ["info", "danger", "warning", "success"];
-                if (types.some(function (rc) {
-                    return type === rc;
-                })) {
-                    $scope.alerts.push({type: type, msg: msg});
-                }
+        //Self
+        var self = this;
+
+        //Mensajes
+        function showMessage(msg, type) {
+            var types = ["info", "danger", "warning", "success"];
+            if (types.some(function (rc) {
+                return type === rc;
+            })) {
+                $scope.alerts.push({type: type, msg: msg});
             }
+        }
 
-            this.showError = function (msg) {
-                showMessage(msg, "danger");
-            };
+        //Response Error
+        function responseError(response) {
+            self.showError(response.data);
+        }
 
-            var self = this;
-            function responseError(response) {
-                self.showError(response.data);
-            }
+        //Close Alert
+        this.closeAlert = function (index) {
+            $scope.alerts.splice(index, 1);
+        };
 
-            //Variables para el controlador
-            this.readOnly = false;
-            this.editMode = false;
+        //Show Error
+        this.showError = function (msg) {
+            showMessage(msg, "danger");
+        };
 
-            this.changeTab = function (tab) {
-                $scope.tab = tab;
-            };
+        //Change Tab
+        this.changeTab = function (tab) {
+            $scope.tab = tab;
+        };
 
-            //Ejemplo alerta
-            showMessage("Bienvenido!, Esto es un ejemplo para mostrar un mensaje de atención", "warning");
+        this.createCiudad = function () {
+            $scope.$broadcast("pre-create", $scope.currentCiudad);
+            this.editMode = true;
+            $scope.currentCiudad = {};
+            $scope.$broadcast("post-create", $scope.currentCiudad);
+        };
 
+        this.editCiudad = function (ciudad) {
+            $scope.$broadcast("pre-edit", $scope.currentCiudad);
+            return svc.fetchCiudad(ciudad.id).then(function (response) {
+                $scope.currentCiudad = response.data;
+                self.editMode = true;
+                $scope.$broadcast("post-edit", $scope.currentCiudad);
+                return response;
+            }, responseError);
+        };
 
-            this.createRecord = function () {
-                $scope.$broadcast("pre-create", $scope.currentRecord);
-                this.editMode = true;
-                $scope.currentRecord = {};
-                $scope.$broadcast("post-create", $scope.currentRecord);
-            };
+        this.fetchCiudades = function () {
+            return svc.fetchCiudades().then(function (response) {
+                $scope.ciudades = response.data;
+                $scope.currentCiudad = {};
+                self.editMode = false;
+                return response;
+            }, responseError);
+        };
 
-            this.editRecord = function (record) {
-                $scope.$broadcast("pre-edit", $scope.currentRecord);
-                return svc.fetchRecord(record.id).then(function (response) {
-                    $scope.currentRecord = response.data;
-                    self.editMode = true;
-                    $scope.$broadcast("post-edit", $scope.currentRecord);
-                    return response;
-                }, responseError);
-            };
+        this.fetchSitios = function (ciudad) {
+            return svc.fetchSitios(ciudad.id).then(function (response) {
+                $scope.sitios = response.data;
+                $scope.currentSitio = {};
+                self.editMode = false;
+                return response;
+            }, responseError);
+        };
 
-            this.fetchCiudades = function () {
-                return svc.fetchCiudades().then(function (response) {
-                    $scope.records = response.data;
-                    $scope.currentRecord = {};
-                    self.editMode = false;
-                    return response;
-                }, responseError);
-            };
+        this.fetchEventos = function (ciudad) {
+            return svc.fetchEventos(ciudad.id).then(function (response) {
+                $scope.eventos = response.data;
+                $scope.currentEvento = {};
+                self.editMode = false;
+                return response;
+            }, responseError);
+        };
 
-             this.fetchSitios = function (id) {
-                return svc.fetchSitios(id).then(function (response) {
-                    $scope.sitios = response.data;
-                    $scope.currentSitio = {};
-                    self.editMode = false;
-                    return response;
-                }, responseError);
-            };
+        this.fetchHoteles = function (ciudad) {
+            return svc.fetchHoteles(ciudad.id).then(function (response) {
+                $scope.hoteles = response.data;
+                $scope.currentHotel = {};
+                self.editMode = false;
+                return response;
+            }, responseError);
+        };
 
-            this.fetchEventos = function () {
-                return svc.fetchSitios.then(function (response) {
-                    $scope.sitios = response.data;
-                    $scope.currentEvento = {};
-                    self.editMode = false;
-                    return response;
-                }, responseError);
-            };
-            this.saveCiudad = function () {
-                return svc.saveCiudad($scope.currentRecord).then(function () {
-                    self.fetchCiudades();
-                }, responseError);
-            };
-            this.saveEvento = function () {
-                return svc.saveEvento($scope.currentRecord).then(function () {
-                    self.fetchEventos();
-                }, responseError);
-            };
-            this.deleteCiudad = function (record) {
-                return svc.deleteCiudad(record.id).then(function () {
-                    self.fetchCiudades();
-                }, responseError);
-            };
-            this.deleteEvento = function (record) {
-                return svc.deleteEvento(record.id).then(function () {
-                    self.fetchEventos();
-                }, responseError);
-            };
+        this.saveCiudad = function () {
+            return svc.saveCiudad($scope.currentCiudad).then(function () {
+                self.fetchCiudades();
+            }, responseError);
+        };
 
-            this.fetchCiudades();
-        }]);
+        this.saveSitio = function (sitio) {
+            return svc.saveSitio($scope.currentCiudad, sitio.id).then(function () {
+                self.fetchSitios();
+            }, responseError);
+        };
+
+        this.saveEvento = function (evento) {
+            return svc.saveSitio($scope.currentCiudad, evento.id).then(function () {
+                self.fetchEventos();
+            }, responseError);
+        };
+
+        this.saveHotel = function (hotel) {
+            return svc.saveHotel($scope.currentCiudad, hotel.id).then(function () {
+                self.fetchHoteles();
+            }, responseError);
+        };
+
+        this.deleteCiudad = function (ciudad) {
+            return svc.deleteCiudad(ciudad.id).then(function () {
+                self.fetchCiudades();
+            }, responseError);
+        };
+
+        this.deleteSitio = function (ciudad, sitio) {
+            return svc.deleteSitio(ciudad.id, sitio.id).then(function () {
+                self.fetchSitios();
+            }, responseError);
+        };
+
+        this.deleteEvento = function (ciudad, evento) {
+            return svc.deleteEvento(ciudad.id, evento.id).then(function () {
+                self.fetchEventos();
+            }, responseError);
+        };
+
+        this.deleteHotel = function (ciudad, hotel) {
+            return svc.deleteHotel(ciudad.id, hotel.id).then(function () {
+                self.fetchEventos();
+            }, responseError);
+        };
+
+        this.fetchCiudades();
+        this.fetchSitios($scope.currentCiudad);
+        this.fetchEventos($scope.currentCiudad);
+        this.fetchHoteles($scope.currentCiudad);
+    }]);
 
 })(window.angular);
--- aplicacion/misVacaciones/src/main/webapp/src/modules/ciudad/ciudad.svc.js
+++ aplicacion/misVacaciones/src/main/webapp/src/modules/ciudad/ciudad.svc.js
@@ -60,7 +60,6 @@
          */
         this.fetchCiudad= function (id) {
             return $http.get(context + "/" + id);
-
         };
 
         /**
@@ -96,13 +95,13 @@
          * Hace una petición GET a /ciudades/:idCiudad/sitios/:idSitio para obtener
          * el objeto de un registro específico de eventos
          * @param {number} idCiudad del registro a obtener
-         * @param {number} idEvento del registro a obtener
+         * @param {number} idHotel del registro a obtener
          * @returns {promise} promise para leer la respuesta del servidor.
          * Se recibe un objeto instancia de evento.
          */
         this.fetchHotel = function(idCiudad,idHotel)
         {
-            return $http.get(context+"/"+ idCiudad +"/"+ "hoteles/" + idEvento);
+            return $http.get(context+"/"+ idCiudad +"/"+ "hoteles/" + idHotel);
         };
 
         /**
@@ -122,7 +121,6 @@
                 return $http.post(context, currentRecord);
             }
         };
-
 
          /** REVISAR DE ACA*/
 
@@ -198,109 +196,5 @@
             return $http.delete(context+"/"+ idCiudad + "/"+ "hoteles"+"/"+ idHotel);
         };
 
-    }]);
-})(window.angular);
-(function (ng) {
-    var mod = ng.module("moduloCiudad");
-
-    mod.service("ciudadService", ["$http", "ciudadContext", function ($http, context) {
-
-        /**
-         * Obtener la lista de authors.
-         * Hace una petición GET con $http a /ciudades para obtener la lista
-         * de objetos de la entidad ciudades
-         * @returns {promise} promise para leer la respuesta del servidor.
-         * Se recibe un array de objetos de ciudades.
-         */
-        this.fetchCiudades = function () {
-            return $http.get(context);
-        };
-
-        this.fetchSitios= function(idCiudad)
-        {
-            return $http.get(context+ "/" + idCiudad + "/"+ "sitios");
-        };
-        this.fetchEventos=function(idCiudad)
-        {
-            return $http.get(context+ "/" + idCiudad + "/"+ "eventos");
-        };
-
-        /**
-         * Obtener un registro de authors.
-         * Hace una petición GET a /authors/:id para obtener
-         * el objeto de un registro específico de authors
-         * @param {number} id del registro a obtener
-         * @returns {promise} promise para leer la respuesta del servidor.
-         * Se recibe un objeto instancia de authors.
-         */
-        this.fetchCiudad= function (id) {
-            return $http.get(context + "/" + id);
-
-        };
-
-
-        this.fetchSitio = function(idCiudad,idSitio)
-        {
-            return $http.get(context+"/"+ idCiudad +"/"+ "sitios"+ "/" + idSitio);
-        };
-        this.fetchEvento = function(idCiudad,idEvento)
-        {
-            return $http.get(context+"/"+ idCiudad +"/"+ "eventos"+ "/" + idEvento);
-        };
-        /**
-         * Guardar un registro de authors.
-         * Si currentRecord tiene la propiedad id, hace un PUT a /authors/:id con los
-         * nuevos datos de la instancia de authors.
-         * Si currentRecord no tiene la propiedad id, se hace un POST a /authors
-         * para crear el nuevo registro de authors
-         * @param {object} currentRecord instancia de authors a guardar/actualizar
-         * @returns {promise} promise para leer la respuesta del servidor.
-         * Se recibe un objeto de authors con su nuevo id
-         */
-        this.saveCiudad = function (currentRecord) {
-            if (currentRecord.id) {
-                return $http.put(context + "/" + currentRecord.id, currentRecord);
-            } else {
-                return $http.post(context, currentRecord);
-            }
-        };
-
-            this.saveSitio = function (currentRecord, idSitio) {
-            if (currentRecord.id) {
-                return $http.put(context + "/" + currentRecord.id+ "/"+ "sitios"+ idSitio, currentRecord);
-            } else {
-                return $http.post(context+ "/"+currentRecord.id+"/"+"sitios", currentRecord);
-            }
-        };
-
-        this.saveEvento = function (currentRecord, idEvento) {
-            if (currentRecord.id) {
-                return $http.put(context + "/" + currentRecord.id+ "/"+ "eventos"+ idEvento, currentRecord);
-            } else {
-                return $http.post(context+ "/"+currentRecord.id+"/"+"eventos", currentRecord);
-            }
-        };
-
-
-
-        /**
-         * Hace una petición DELETE a /authors/:id para eliminar un author
-         * @param {number} id identificador de la instancia de author a eliminar
-         * @returns {promise} promise para leer la respuesta del servidor.
-         * No se recibe cuerpo en la respuesta.
-         */
-        this.deleteCiudad = function (id) {
-            return $http.delete(context + "/" + id);
-        };
-
-
-
-         this.deleteSitio = function (idCiudad, idSitio) {
-            return $http.delete(context+"/"+ idCiudad + "/"+ "sitios"+"/"+ idSitio);
-        };
-
-         this.deleteEvento = function (idCiudad, idEvento) {
-            return $http.delete(context+"/"+ idCiudad + "/"+ "eventos"+"/"+ idEvento);
-        };
     }]);
 })(window.angular);
