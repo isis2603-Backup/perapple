1566ae72925e979c728c6bafd2b5d16be21709eb
Daniel Althviz Moré <d.althviz10@uniandes.edu.co>
2016-03-03 12:31:40 -0500

    eliminada carpeta de aplicación anterior

--- aplicacion/misVacaciones/src/main/webapp/src/modules/ciudad/ciudad.mod.js
+++
@@ -1,6 +0,0 @@
-(function (ng) {
-    var mod = ng.module("moduloCiudad", ["ui.bootstrap"]);
-
-    mod.constant("ciudadContext", "api/ciudades");
-
-})(window.angular);
--- aplicacion/misVacaciones/src/main/webapp/src/modules/itinerario/itinerario.mod.js
+++
@@ -1,6 +0,0 @@
-(function (ng) {
-    var mod = ng.module("itinerarioModule", ["ui.bootstrap"]);
-
-    mod.constant("itinerarioContext", "api/itinerario");
-
-})(window.angular);
