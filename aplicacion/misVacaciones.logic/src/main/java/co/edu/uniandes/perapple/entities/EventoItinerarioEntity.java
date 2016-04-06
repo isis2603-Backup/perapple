d200d3aa8c4fc3632f2eadd7ea08a46deef58e11
camendoza94 <ca.mendoza968@uniandes.edu.co>
2016-04-06 14:39:45 -0500

    EventoItinerarioEntity + SitioItinerarioEntity

--- aplicacion/misVacaciones.logic/src/main/java/co/edu/uniandes/perapple/entities/EventoItinerarioEntity.java
+++ aplicacion/misVacaciones.logic/src/main/java/co/edu/uniandes/perapple/entities/EventoItinerarioEntity.java
@@ -1,9 +1,56 @@
 package co.edu.uniandes.perapple.entities;
 
+import java.util.Date;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
 /**
  *
  * @author camen
  */
 public class EventoItinerarioEntity {
+    @ManyToOne
+    private EventoEntity evento;
     
+    @ManyToOne
+    private CiudadItinerarioEntity ciudad;
+    
+    @Temporal(TemporalType.DATE)
+    private Date fechaIni;
+    
+    @Temporal(TemporalType.DATE)
+    private Date fechaFin;
+
+    public EventoEntity getEvento() {
+        return evento;
+    }
+
+    public void setEvento(EventoEntity evento) {
+        this.evento = evento;
+    }
+
+    public CiudadItinerarioEntity getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(CiudadItinerarioEntity ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public Date getFechaIni() {
+        return fechaIni;
+    }
+
+    public void setFechaIni(Date fechaIni) {
+        this.fechaIni = fechaIni;
+    }
+
+    public Date getFechaFin() {
+        return fechaFin;
+    }
+
+    public void setFechaFin(Date fechaFin) {
+        this.fechaFin = fechaFin;
+    }
 }
