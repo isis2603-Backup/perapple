396ef4d5e7439541404f6eaf2333e384a0713ca3
Esteban Dalel R <estebandalelr@gmail.com>
2016-04-06 06:51:58 -0500

    Completar métodos de viajero en ViajeroLogic

--- aplicacion/misVacaciones.logic/src/main/java/co/edu/uniandes/perapple/ejbs/ViajeroLogic.java
+++ aplicacion/misVacaciones.logic/src/main/java/co/edu/uniandes/perapple/ejbs/ViajeroLogic.java
@@ -26,27 +26,63 @@ public class ViajeroLogic implements IViajeroLogic {
 
     @Override
     public List<ViajeroEntity> getItinerarios() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        logger.info("Inicia proceso de consultar todas los viajeros.");
+        List<ViajeroEntity> viajeros = persistence.findAll();
+        logger.info("Termina proceso de consultar todos los libros");
+
+        return viajeros;
     }
 
     @Override
     public ViajeroEntity getViajero(Long id) throws BusinessLogicException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    logger.log(Level.INFO, "Inicia proceso de consultar viajero con id={0}", id);
+        ViajeroEntity viajero = persistence.find(id);
+        if (viajero == null) {
+            logger.log(Level.SEVERE, "El viajero con el id {0} no existe", id);
+            throw new BusinessLogicException("El viajero solicitado no existe");
+        }
+        logger.log(Level.INFO, "Termina proceso de consultar El viajero con id={0}", id);
+        return viajero;
     }
 
     @Override
-    public ViajeroEntity createViajero(ViajeroEntity entity) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public ViajeroEntity createViajero(ViajeroEntity entity) throws BusinessLogicException{
+    //Validaciones:
+        //1. El viajero a crear no existe.
+        //2. El nuevo ID a asignar al viajero está disponible.
+        logger.info("Inicia proceso de creación de un viajero");
+        if (validateViajeroExiste(entity.getId())) {
+            throw new BusinessLogicException("Ya existe un viajero con ese id. No se puede crear. ");
+        }
+        persistence.create(entity);
+        logger.info("Termina proceso de creación del viajero");
+        return entity;
     }
 
     @Override
-    public ViajeroEntity updateViajero(ViajeroEntity entity) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public ViajeroEntity updateViajero(ViajeroEntity entity) throws BusinessLogicException{
+        //Validaciones
+        //1. El viajero ya existe.
+        //2. No se actualiza el ID.
+        logger.log(Level.INFO, "Inicia proceso de actualizar viajero con id={0}", entity.getId());
+        if (!validateViajeroExiste(entity.getId())) {
+            throw new BusinessLogicException("el viajero que se quiere actualizar no existe.");
+        }
+        ViajeroEntity newEntity = persistence.update(entity);
+        logger.log(Level.INFO, "Termina proceso de actualizar viajero con id={0}", entity.getId());
+        return newEntity;
     }
 
     @Override
     public void deleteViajero(Long id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //Validaciones
+        //1. El viajero ya existe.
+        logger.log(Level.INFO, "Inicia proceso de borrar viajero con id={0}", id);
+        if (!validateViajeroExiste(id)) {
+            throw new BusinessLogicException("el viajero que se quiere eliminar no existe.");
+        }
+        persistence.delete(id);
+        logger.log(Level.INFO, "Termina proceso de borrar viajero con id={0}", id);
     }
 
     @Override
