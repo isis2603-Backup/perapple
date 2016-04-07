f03e6860be4188d385eb0588eec09bdd264f05b6
Esteban Dalel R <estebandalelr@gmail.com>
2016-04-07 10:01:36 -0500

    Cambios a viajeroEntity, settergetters

--- aplicacion/misVacaciones.logic/src/main/java/co/edu/uniandes/perapple/entities/ViajeroEntity.java
+++ aplicacion/misVacaciones.logic/src/main/java/co/edu/uniandes/perapple/entities/ViajeroEntity.java
@@ -10,18 +10,71 @@ import javax.persistence.ManyToMany;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.persistence.Id;
+import sun.security.util.Password;
 
 @Entity
 public class ViajeroEntity extends BaseEntity implements Serializable {
 
 
-  //TODO implementar metodos get y set por todos los atributos de la entidad -- agregar relaciones con otras entidades utilizando anotaciones
+  //TODO  -- agregar relaciones con otras entidades utilizando anotaciones
     @Id
     private int id;
 
-    public int getIdentificador()
+    public int getId()
     {
         return id;
     }
+    public void setId(int id)
+    {
+        this.id=id;
+    }
 
+    @Name
+    private String name;
+
+    public String getName()
+    {
+        return name;
+    }
+    public void setName(String name)
+    {
+        this.name=name;
+    }
+
+    @Email
+    private String email
+
+     public String getEmail()
+     {
+         return email;
+     }
+     public void setEmail(String email)
+     {
+         this.email=email;
+     }
+
+    @Password
+    private String password;
+
+    public String getPassword()
+    {
+        return password;
+
+    }
+    public void setPassword(String password)
+    {
+        this.password=password;
+    }
+
+    @Image
+    private String image;
+
+    public String getImage()
+    {
+        return image;
+    }
+    public void setImage(String image)
+    {
+        this.image=image;
+    }
 }
