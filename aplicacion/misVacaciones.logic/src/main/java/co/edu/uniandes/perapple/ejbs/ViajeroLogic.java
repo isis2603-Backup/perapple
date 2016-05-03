4b5c48acd34d8577badd6520365345c492ec3426
ca.mendoza968 <ca.mendoza968@172.24.99.225>
2016-05-03 07:44:40 -0500

    Cambios. Issues resueltos.

--- aplicacion/misVacaciones.logic/src/main/java/co/edu/uniandes/perapple/ejbs/ViajeroLogic.java
+++ aplicacion/misVacaciones.logic/src/main/java/co/edu/uniandes/perapple/ejbs/ViajeroLogic.java
@@ -13,60 +13,58 @@ import co.edu.uniandes.perapple.api.IViajeroLogic;
 @Stateless
 public class ViajeroLogic implements IViajeroLogic {
 
-    private static final Logger logger = Logger.getLogger(ViajeroLogic.class.getName());
+    private static final Logger LOGGER = Logger.getLogger(ViajeroLogic.class.getName());
 
     @Inject
     private ViajeroPersistence persistence;
 
-
-     //Posibilidad de inyectar mas instancias de las otras persistencias de ser necesario
-
+    //Posibilidad de inyectar mas instancias de las otras persistencias de ser necesario
     @Override
     public List<ViajeroEntity> getViajeros() {
-        logger.info("Inicia proceso de consultar todas los viajeros.");
+        LOGGER.info("Inicia proceso de consultar todas los viajeros.");
         List<ViajeroEntity> viajeros = persistence.findAll();
-        logger.info("Termina proceso de consultar todos los libros");
+        LOGGER.info("Termina proceso de consultar todos los libros");
 
         return viajeros;
     }
 
     @Override
     public ViajeroEntity getViajero(int id) throws BusinessLogicException {
-    logger.log(Level.INFO, "Inicia proceso de consultar viajero con id={0}", id);
+        LOGGER.log(Level.INFO, "Inicia proceso de consultar viajero con id={0}", id);
         ViajeroEntity viajero = persistence.find(id);
         if (viajero == null) {
-            logger.log(Level.SEVERE, "El viajero con el id {0} no existe", id);
+            LOGGER.log(Level.SEVERE, "El viajero con el id {0} no existe", id);
             throw new BusinessLogicException("El viajero solicitado no existe");
         }
-        logger.log(Level.INFO, "Termina proceso de consultar El viajero con id={0}", id);
+        LOGGER.log(Level.INFO, "Termina proceso de consultar El viajero con id={0}", id);
         return viajero;
     }
 
     @Override
-    public ViajeroEntity createViajero(ViajeroEntity entity) throws BusinessLogicException{
-    //Validaciones:
+    public ViajeroEntity createViajero(ViajeroEntity entity) throws BusinessLogicException {
+        //Validaciones:
         //1. El viajero a crear no existe.
         //2. El nuevo ID a asignar al viajero está disponible.
-        logger.info("Inicia proceso de creación de un viajero");
+        LOGGER.info("Inicia proceso de creación de un viajero");
         if (validateViajeroExiste(entity.getId())) {
             throw new BusinessLogicException("Ya existe un viajero con ese id. No se puede crear. ");
         }
         persistence.create(entity);
-        logger.info("Termina proceso de creación del viajero");
+        LOGGER.info("Termina proceso de creación del viajero");
         return entity;
     }
 
     @Override
-    public ViajeroEntity updateViajero(ViajeroEntity entity) throws BusinessLogicException{
+    public ViajeroEntity updateViajero(ViajeroEntity entity) throws BusinessLogicException {
         //Validaciones
         //1. El viajero ya existe.
         //2. No se actualiza el ID.
-        logger.log(Level.INFO, "Inicia proceso de actualizar viajero con id={0}", entity.getId());
+        LOGGER.log(Level.INFO, "Inicia proceso de actualizar viajero con id={0}", entity.getId());
         if (!validateViajeroExiste(entity.getId())) {
             throw new BusinessLogicException("el viajero que se quiere actualizar no existe.");
         }
         ViajeroEntity newEntity = persistence.update(entity);
-        logger.log(Level.INFO, "Termina proceso de actualizar viajero con id={0}", entity.getId());
+        LOGGER.log(Level.INFO, "Termina proceso de actualizar viajero con id={0}", entity.getId());
         return newEntity;
     }
 
@@ -74,12 +72,12 @@ public class ViajeroLogic implements IViajeroLogic {
     public void deleteViajero(int id) throws BusinessLogicException {
         //Validaciones
         //1. El viajero ya existe.
-        logger.log(Level.INFO, "Inicia proceso de borrar viajero con id={0}", id);
+        LOGGER.log(Level.INFO, "Inicia proceso de borrar viajero con id={0}", id);
         if (!validateViajeroExiste(id)) {
             throw new BusinessLogicException("el viajero que se quiere eliminar no existe.");
         }
         persistence.delete(id);
-        logger.log(Level.INFO, "Termina proceso de borrar viajero con id={0}", id);
+        LOGGER.log(Level.INFO, "Termina proceso de borrar viajero con id={0}", id);
     }
 
     private boolean validateViajeroExiste(int viajeroId) {
@@ -92,5 +90,4 @@ public class ViajeroLogic implements IViajeroLogic {
 
         return true;
     }
-
 }
--- aplicacion/misVacaciones.logic/src/main/java/co/edu/uniandes/perapple/persistence/ViajeroPersistence.java
+++ aplicacion/misVacaciones.logic/src/main/java/co/edu/uniandes/perapple/persistence/ViajeroPersistence.java
@@ -12,36 +12,36 @@ import javax.persistence.Query;
 @Stateless
 public class ViajeroPersistence {
 
-    private static final Logger logger = Logger.getLogger(ViajeroPersistence.class.getName());
+    private static final Logger LOGGER = Logger.getLogger(ViajeroPersistence.class.getName());
 
     @PersistenceContext(unitName = "MisVacacionesPU")
     protected EntityManager em;
 
     public ViajeroEntity create(ViajeroEntity entity) {
-        logger.info("Creando un viajero nuevo");
+        LOGGER.info("Creando un viajero nuevo");
         em.persist(entity);
-        logger.info("Viajero creado");
+        LOGGER.info("Viajero creado");
         return entity;
     }
 
     public ViajeroEntity update(ViajeroEntity entity) {
-        logger.log(Level.INFO, "Actualizando viajero con id={0}", entity.getId());
+        LOGGER.log(Level.INFO, "Actualizando viajero con id={0}", entity.getId());
         return em.merge(entity);
     }
 
     public void delete(int id) {
-        logger.log(Level.INFO, "Borrando viajero con id={0}", id);
+        LOGGER.log(Level.INFO, "Borrando viajero con id={0}", id);
         ViajeroEntity entity = em.find(ViajeroEntity.class, id);
         em.remove(entity);
     }
 
     public ViajeroEntity find(int id) {
-        logger.log(Level.INFO, "Consultando viajero con id={0}", id);
+        LOGGER.log(Level.INFO, "Consultando viajero con id={0}", id);
         return em.find(ViajeroEntity.class, id);
     }
 
     public List<ViajeroEntity> findAll() {
-        logger.info("Consultando todos los viajeros");
+        LOGGER.info("Consultando todos los viajeros");
         Query q = em.createQuery("select u from ViajeroEntity u");
         return q.getResultList();
     }
