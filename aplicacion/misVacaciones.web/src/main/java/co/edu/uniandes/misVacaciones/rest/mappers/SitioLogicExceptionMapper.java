2910f4a7fd0c1ed6c3bcd19547572827346c08c7
ca.mendoza968 <ca.mendoza968@172.24.99.225>
2016-05-05 04:28:34 -0500

    Cambios. Issues resueltos.

--- aplicacion/misVacaciones.web/src/main/java/co/edu/uniandes/misVacaciones/rest/mappers/SitioLogicExceptionMapper.java
+++ aplicacion/misVacaciones.web/src/main/java/co/edu/uniandes/misVacaciones/rest/mappers/SitioLogicExceptionMapper.java
@@ -12,18 +12,19 @@ import co.edu.uniandes.misVacaciones.rest.exceptions.SitioLogicException;
 @Provider
 public class SitioLogicExceptionMapper implements ExceptionMapper<SitioLogicException> {
 
-	/**
-	 * Generador de una respuesta a partir de una excepción
-	 * @param ex excecpión a convertir a una respuesta REST
-	 */
-	@Override
-	public Response toResponse(SitioLogicException ex) {
-		// retorna una respuesta
-		return Response
-				.status(Response.Status.NOT_FOUND)	// estado HTTP 404
-				.entity(ex.getMessage())			// mensaje adicional
-				.type("text/plain")
-				.build();
-	}
+    /**
+     * Generador de una respuesta a partir de una excepción
+     *
+     * @param ex excecpión a convertir a una respuesta REST
+     */
+    @Override
+    public Response toResponse(SitioLogicException ex) {
+        // retorna una respuesta
+        return Response
+                .status(Response.Status.NOT_FOUND) // estado HTTP 404
+                .entity(ex.getMessage()) // mensaje adicional
+                .type("text/plain")
+                .build();
+    }
 
 }
