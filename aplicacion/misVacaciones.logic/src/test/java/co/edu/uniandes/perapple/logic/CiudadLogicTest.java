716b7f4d590d761aefec7ef5ace9943391e73671
Harold Esteban González Morales <he.gonzalez10@uniandes.edu.co>
2016-05-15 04:17:06 -0500

    más pruebas en CiudadLogicTest

--- aplicacion/misVacaciones.logic/src/test/java/co/edu/uniandes/perapple/logic/CiudadLogicTest.java
+++ aplicacion/misVacaciones.logic/src/test/java/co/edu/uniandes/perapple/logic/CiudadLogicTest.java
@@ -27,6 +27,8 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import uk.co.jemos.podam.api.PodamFactory;
 import uk.co.jemos.podam.api.PodamFactoryImpl;
@@ -38,6 +40,9 @@ import uk.co.jemos.podam.api.PodamFactoryImpl;
 @RunWith(Arquillian.class)
 public class CiudadLogicTest {
 
+     @Rule
+    public ExpectedException exception = ExpectedException.none();
+    
     /**
      * Generador de info aleatoria con Podam
      */
@@ -137,7 +142,6 @@ public class CiudadLogicTest {
         try {
             CiudadEntity expected = factory.manufacturePojo(CiudadEntity.class);
             CiudadEntity created = ciudadLogic.createCiudad(expected);
-
             CiudadEntity result = em.find(CiudadEntity.class, created.getId());
 
             assertNotNull(result);
@@ -146,12 +150,32 @@ public class CiudadLogicTest {
             assertEquals(expected.getNombre(), result.getNombre());
             assertEquals(expected.getDetalles(), result.getDetalles());
             assertEquals(expected.getImagen(), result.getImagen());
-
+          
+            
         } catch (BusinessLogicException ex) {
             fail(ex.getLocalizedMessage());
         }
-    }
+        
+       }
 
+    @Test
+    public void createCiudadTest2()
+    {
+        try
+        {
+        CiudadEntity expected = factory.manufacturePojo(CiudadEntity.class);
+        expected.setId(1);    
+       ciudadLogic.createCiudad(expected);
+                             
+        }
+        catch(BusinessLogicException ex)
+        {
+            fail(ex.getLocalizedMessage());
+        }
+    }
+    
+     
+    
     @Test
     public void getCiudadesTest() {
         List<CiudadEntity> resultList = ciudadLogic.getCiudades();
@@ -188,6 +212,19 @@ public class CiudadLogicTest {
         }
     }
 
+     @Test
+    public void getciudadTest2(){
+        try{
+            ciudadLogic.getCiudad(3764);
+            fail("stop");
+        }catch (BusinessLogicException ex) {
+            assertTrue(true);
+        } catch (Exception ex ){
+            System.out.println("erorrr: "+ex.getMessage());
+            fail(ex.getLocalizedMessage());
+        }
+    }
+    
     @Test
     public void deleteCiudadTest() {
         try {
@@ -197,6 +234,24 @@ public class CiudadLogicTest {
             assertNull(deleted);
 
         } catch (BusinessLogicException ex) {
+            fail(ex.getLocalizedMessage());
+        }
+    }
+    
+    @Test
+    public void deleteCiudadTest2(){
+        try{
+            CiudadEntity ciudad = data.get(0);
+            
+            ciudad.setId(3455);
+            
+            ciudadLogic.deleteCiudad(ciudad.getId());
+
+            fail("stop");
+            
+        }catch (BusinessLogicException ex) {
+            assertTrue(true);
+        } catch (Exception ex ){
             fail(ex.getLocalizedMessage());
         }
     }
@@ -221,7 +276,53 @@ public class CiudadLogicTest {
             fail(ex.getLocalizedMessage());
         }
     }
+    
+    @Test
+    public void updateCiudadTest2()  {
+        try{
+           CiudadEntity ciudad= ciudadLogic.getCiudades().get(0);
+           ciudad.setId(2002);
+           ciudadLogic.updateCiudad(ciudad);
+           
+           fail("stop");
+        }           
+       catch (BusinessLogicException ex) {
+            assertTrue(true);
+        } catch (Exception ex ){
+            fail(ex.getLocalizedMessage());
+        }
+    }
+    
+     @Test
+    public void getEventosTest(){
+        try{
+            CiudadEntity ciudadABuscarEventos = data.get(0);
+            
+            List<EventoEntity> lista = ciudadLogic.getEventos(ciudadABuscarEventos.getId());
 
+            assertEquals(1, lista.size());
+            
+        }catch (BusinessLogicException ex) {
+            fail(ex.getLocalizedMessage());
+        } catch (Exception ex ){
+            fail(ex.getLocalizedMessage());
+        }
+    }
+
+    @Test
+    public void getEventosTest2(){
+        try{
+            ciudadLogic.getEventos(5899);
+
+            fail("stop");
+            
+        }catch (BusinessLogicException ex) {
+            assertTrue(true);
+        } catch (Exception ex ){
+            fail(ex.getLocalizedMessage());
+        }
+    }
+    
     @Test
     public void getEventoTest() {
         try {
@@ -239,6 +340,88 @@ public class CiudadLogicTest {
             assertEquals(expected.getImagen(), response.getImagen());
             assertEquals(expected.getFechaEvento(), response.getFechaEvento());
         } catch (BusinessLogicException ex) {
+            fail(ex.getLocalizedMessage());
+        }
+    }
+    
+    @Test 
+    public void getEventoTest2()
+    {
+         try{
+           ciudadLogic.getEvento(23567, 0);
+
+            fail("stop");
+            
+        }catch (BusinessLogicException ex) {
+            assertTrue(true);
+        } catch (Exception ex ){
+            fail(ex.getLocalizedMessage());
+        }
+    }
+    
+    @Test
+    public void addEventoTest()
+    {
+        try{
+            CiudadEntity ciudad = data.get(0);
+            EventoEntity evento = eventosData.get(2);
+                                    
+            ciudadLogic.addEvento(ciudad.getId(), evento);
+            
+            assertEquals(ciudad.getEventos().size(), ciudadLogic.getEventos(ciudad.getId()).size());
+        }
+        catch(BusinessLogicException ex)
+                {
+                    fail(ex.getLocalizedMessage());
+                 }
+    }
+    
+    @Test
+    public void addEventoTest2(){
+        try{
+            CiudadEntity ciudad =  data.get(0);
+            ciudad.setId(1244);
+            EventoEntity evento = eventosData.get(2);
+            ciudadLogic.addEvento(ciudad.getId(), evento);
+            fail("stop");
+            
+        }
+        catch(BusinessLogicException ex)
+        {
+            assertTrue(true);
+        }
+        catch(Exception ex)
+        {
+            fail(ex.getLocalizedMessage());
+        }
+    }
+    
+    @Test
+    public void getSitiosTest(){
+        try{
+            CiudadEntity ciudadABuscarSitios = data.get(0);
+            
+            List<SitioEntity> lista = ciudadLogic.getSitios(ciudadABuscarSitios.getId());
+
+            assertEquals(1, lista.size());
+            
+        }catch (BusinessLogicException ex) {
+            fail(ex.getLocalizedMessage());
+        } catch (Exception ex ){
+            fail(ex.getLocalizedMessage());
+        }
+    }
+    
+    @Test
+    public void getSitiosTest2(){
+        try{
+            ciudadLogic.getSitios(5899);
+
+            fail("stop");
+            
+        }catch (BusinessLogicException ex) {
+            assertTrue(true);
+        } catch (Exception ex ){
             fail(ex.getLocalizedMessage());
         }
     }
@@ -264,6 +447,59 @@ public class CiudadLogicTest {
         }
     }
 
+    @Test 
+    public void getSitioTest2()
+    {
+         try{
+            ciudadLogic.getSitio(23567, 0);
+
+            fail("stop");
+            
+        }catch (BusinessLogicException ex) {
+            assertTrue(true);
+        } catch (Exception ex ){
+            fail(ex.getLocalizedMessage());
+        }
+    }
+    
+     @Test
+    public void addSitioTest()
+    {
+        try{
+            CiudadEntity ciudad = data.get(0);
+            SitioEntity sitio = sitiosData.get(2);
+                                    
+            ciudadLogic.addSitio(ciudad.getId(), sitio);
+            
+            assertEquals(ciudad.getSitios().size(), ciudadLogic.getSitios(ciudad.getId()).size());
+        }
+        catch(BusinessLogicException ex)
+                {
+                    fail(ex.getLocalizedMessage());
+                 }
+    }
+    
+    @Test
+    public void addSitioTest2(){
+        try{
+            CiudadEntity ciudad =  data.get(0);
+            ciudad.setId(1244);
+            SitioEntity sitio = sitiosData.get(2);
+            ciudadLogic.addSitio(ciudad.getId(), sitio);
+            fail("stop");
+            
+        }
+        catch(BusinessLogicException ex)
+        {
+            assertTrue(true);
+        }
+        catch(Exception ex)
+        {
+            fail(ex.getLocalizedMessage());
+        }
+    }
+
+        
     @Test
     public void replaceEventoTest() {
 
@@ -291,6 +527,25 @@ public class CiudadLogicTest {
     }
 
     @Test
+    public void replaceEventoTest2(){
+        try{
+            CiudadEntity ciudad = data.get(0);
+            
+            List<EventoEntity> lista = ciudad.getEventos();
+            
+            EventoEntity eventoAActualizar = lista.get(0);
+            
+            ciudadLogic.updateEvento(4332,eventoAActualizar.getId(), eventoAActualizar);
+            
+            fail("stop");
+        }catch (BusinessLogicException ex) {
+            assertTrue(true);
+        } catch (Exception ex ){
+            fail(ex.getLocalizedMessage());
+        }
+    }
+    
+    @Test
     public void replaceSitioTest() {
         try {
             SitioEntity entity = sitiosData.get(0);
@@ -315,12 +570,67 @@ public class CiudadLogicTest {
         }
     }
 
+     @Test
+    public void replaceSitioTest2(){
+        try{
+            CiudadEntity ciudad = data.get(0);
+            
+            List<SitioEntity> lista = ciudad.getSitios();
+            
+            SitioEntity sitioAActualizar = lista.get(0);
+            
+            ciudadLogic.updateSitio(4332,sitioAActualizar.getId(), sitioAActualizar);
+            
+            fail("stop");
+        }catch (BusinessLogicException ex) {
+            assertTrue(true);
+        } catch (Exception ex ){
+            fail(ex.getLocalizedMessage());
+        }
+    }
+    
     @Test
     public void removeEventoTest() {
         try {
             ciudadLogic.removeEvento(eventosData.get(0).getId(), data.get(0).getId());
 
         } catch (BusinessLogicException ex) {
+            fail(ex.getLocalizedMessage());
+        }
+    }
+    
+       @Test
+    public void removeEventoTest2(){
+        try{
+            CiudadEntity ciudad = data.get(0);
+            
+            EventoEntity evento = ciudad.getEventos().get(0);
+            
+           ciudadLogic.removeEvento(evento.getId(), 466);
+           
+            fail("No debería pasar de acá porque la ciudad no existe");
+            
+        }catch (BusinessLogicException ex) {
+            assertTrue(true);
+        } catch (Exception ex ){
+            fail(ex.getLocalizedMessage());
+        }
+    }
+    
+      @Test
+    public void removeEventoTest3(){
+        try{
+            CiudadEntity ciudad = data.get(0);
+            
+            EventoEntity evento = ciudad.getEventos().get(0);
+            
+           ciudadLogic.removeEvento(455, ciudad.getId());
+           
+            fail("No debería pasar de acá porque el evento no existe en esa ciudad");
+            
+        }catch (BusinessLogicException ex) {
+            assertTrue(true);
+        } catch (Exception ex ){
             fail(ex.getLocalizedMessage());
         }
     }
@@ -334,6 +644,43 @@ public class CiudadLogicTest {
             fail(ex.getLocalizedMessage());
         }
     }
+    
+     @Test
+    public void removeSitioTest2(){
+        try{
+            CiudadEntity ciudad = data.get(0);
+            
+            SitioEntity sitio = ciudad.getSitios().get(0);
+            
+           ciudadLogic.removeSitio(sitio.getId(), 466);
+           
+            fail("No debería pasar de acá porque la ciudad no existe");
+            
+        }catch (BusinessLogicException ex) {
+            assertTrue(true);
+        } catch (Exception ex ){
+            fail(ex.getLocalizedMessage());
+        }
+    }
+    
+      @Test
+    public void removeSitioTest3(){
+        try{
+            CiudadEntity ciudad = data.get(0);
+            
+            SitioEntity sitio = ciudad.getSitios().get(0);
+            
+           ciudadLogic.removeSitio(455, ciudad.getId());
+           
+            fail("No debería pasar de acá porque el sitio no existe en esa ciudad");
+            
+        }catch (BusinessLogicException ex) {
+            assertTrue(true);
+        } catch (Exception ex ){
+            fail(ex.getLocalizedMessage());
+        }
+    }
+  
 
     private EventoEntity getCiudadEvento(int ciudadId, int eventoId) {
         Query q = em.createQuery("Select DISTINCT e from CiudadEntity c join c.eventos e where c.id = :ciudadId and e.id=:eventoId");
